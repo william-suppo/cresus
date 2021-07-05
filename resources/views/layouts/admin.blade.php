778bc60601370fb64f4af365f0f26c13399d695d
William <work@suppo.fr>
2021-07-05 22:26:13 +0200

    Correction du design des menus de la sidebar.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,13 +15,13 @@
         <aside class="bg-blue-900 md:col-span-2">
             <div class="w-full text-center py-8 text-3xl text-white uppercase font-semibold">{{ __('Cresus') }}</div>
             <nav class="text-white">
-                <a href="#" class="{{ Request::is('dashboard') ? 'bg-blue-800 opacity-100 text-blue-900' : '' }} flex items-center opacity-80 hover:opacity-100 hover:bg-gray-100 hover:text-blue-900 py-4 pl-6">
+                <a href="#" class="{{ Request::is('dashboard') ? 'bg-gray-100 opacity-100 text-blue-900' : '' }} flex items-center opacity-80 hover:opacity-100 hover:bg-gray-100 hover:text-blue-900 py-4 pl-6">
                     <svg class="h-6 w-6 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
                     </svg>
                     {{ __('Dashboard') }}
                 </a>
-                <a href="#" class="{{ Request::is('accounts*') ? 'bg-blue-800 opacity-100 text-blue-900' : '' }} flex items-center opacity-80 hover:opacity-100 hover:bg-gray-100 hover:text-blue-900 py-4 pl-6">
+                <a href="#" class="{{ Request::is('accounts*') ? 'bg-gray-100 opacity-100 text-blue-900' : '' }} flex items-center opacity-80 hover:opacity-100 hover:bg-gray-100 hover:text-blue-900 py-4 pl-6">
                     <svg class="h-6 w-6 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
                     </svg>
